4bf1e01dbe2026938298b701e12feda37baf0969
Spencer Collins <CollinsSpencer@users.noreply.github.com>
2018-04-05 10:14:00 -0500

    Update Power Method

--- Console/Program.cs
+++ Console/Program.cs
@@ -81,7 +81,6 @@ namespace TravisCILab
             return double.Parse(x) / double.Parse(y);
         }
 
-        // Implement this method following a similar pattern as above
         public static double Power(string x, string y)
         {
             return Math.Pow(double.Parse(x), double.Parse(y));
