05631b406145fe697d092c67c0ad017a0f93e59c
dtines <dptines@gmail.com>
2018-04-05 10:13:20 -0500

    Implemented power method

--- Console/Program.cs
+++ Console/Program.cs
@@ -81,10 +81,9 @@ namespace TravisCILab
             return double.Parse(x) / double.Parse(y);
         }
 
-        // Implement this method following a similar pattern as above
         public static double Power(string x, string y)
         {
-            throw new NotImplementedException();
+            return Math.Pow(double.Parse(x), double.Parse(y));
         }
     }
 
