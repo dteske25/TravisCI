7f9de0ba3fba38c3a97e3d18f6c06a1808d7b362
JarodAert <jarod.aerts@huskers.unl.edu>
2019-03-30 13:50:41 -0500

    Fixed problem

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -108,7 +108,7 @@ namespace TravisCILab
         {
             Assert.AreEqual(1, Program.Power("1", "2"));
             Assert.AreEqual(9, Program.Power("3", "2"));
-            Assert.AreEqual(1255, Program.Power("5", "3"));
+            Assert.AreEqual(125, Program.Power("5", "3"));
         }
 
         [Test]
