6663c64f46451d0aa942b7e5187d5bb2b18a6901
benstuart0 <ben.stuart@huskers.unl.edu>
2019-04-02 00:26:25 -0500

    tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -13,6 +13,7 @@ namespace Tests
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
             Assert.AreEqual(12, Program.Add("5", "7"));
+
         }
 
         [Test]
