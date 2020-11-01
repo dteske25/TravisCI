c1e5d3d252455a77deff68e58b9d7e0cf7c14def
Davey <46133063+rdwinter3@users.noreply.github.com>
2020-10-31 19:12:09 -0500

    added tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -29,7 +29,102 @@ namespace TravisCILab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
+        [Test]
+        public void Subtract_Valid()
+        {
+            Assert.AreEqual(3, Program.Subtract("5", "2"));
+            Assert.AreEqual(8, Program.Subtract("12", "4"));
+            Assert.AreEqual(4, Program.Subtract("9", "5"));
+        }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+        [Test]
+        public void Multiply_Valid()
+        {
+            Assert.AreEqual(56, Program.Multiply("7", "8"));
+            Assert.AreEqual(5, Program.Multiply("5", "1"));
+            Assert.AreEqual(120, Program.Multiply("10", "12"));
+        }
+
+        [Test]
+        public void Multiply_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+        [Test]
+        public void Divide_Valid()
+        {
+            Assert.AreEqual(7, Program.Divide("42", "6"));
+            Assert.AreEqual(11, Program.Divide("121", "11"));
+            Assert.AreEqual(2, Program.Divide("4", "2"));
+        }
+
+        [Test]
+        public void Divide_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+        [Test]
+        public void Power_Valid()
+        {
+            Assert.AreEqual(16, Program.Power("4", "2"));
+            Assert.AreEqual(27, Program.Power("3", "3"));
+            Assert.AreEqual(625, Program.Power("5", "4"));
+        }
+
+        [Test]
+        public void Power_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
+
+
+
+
+        
     }
 }
