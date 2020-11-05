393e877786b9cbc4c6fde37f9b9e899da6238fde
Kelly <kkamrath@cse.unl.edu>
2020-11-05 00:35:14 -0600

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -35,7 +35,7 @@ namespace TravisCILab
         {
             Assert.AreEqual(-1, Program.Subtract("1", "2"));
             Assert.AreEqual(1, Program.Subtract("3", "2"));
-            Assert.AreEqual(-1, Program.Subtract("5", "7"));
+            Assert.AreEqual(-2, Program.Subtract("5", "7"));
         }
 
         [Test]
