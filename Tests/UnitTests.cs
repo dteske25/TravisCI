4d4d0c63b903fbac904b06091db169f2b116863d
Bryce Jen Wei Yong <54676043+bryceyong@users.noreply.github.com>
2020-11-06 17:01:50 -0600

    Unit test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -32,15 +32,87 @@ namespace TravisCILab
 
         // Implement 3 tests per operation, following a similar pattern as above
         [Test]
-        public void Pow_Valid()
+        public void Subtract_Valid()
         {
-            Assert.AreEqual(3, Program.Power("1", "3"));
-            Assert.AreEqual(8, Program.Power("2", "3"));
-            Assert.AreEqual(4, Program.Power("2", "2"));
+            Assert.AreEqual(1, Program.Subtract("1", "0"));
+            Assert.AreEqual(4, Program.Subtract("7", "3"));
+            Assert.AreEqual(23, Program.Subtract("53", "30"));
         }
 
         [Test]
-        public void Pow_Invalid()
+        public void Subtract_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_Valid()
+        {
+            Assert.AreEqual(6, Program.Multiply("1", "6"));
+            Assert.AreEqual(49, Program.Multiply("7", "7"));
+            Assert.AreEqual(24, Program.Multiply("2", "12"));
+        }
+
+        [Test]
+        public void Multiply_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_Valid()
+        {
+            Assert.AreEqual(2, Program.Divide("4", "2"));
+            Assert.AreEqual(3, Program.Divide("9", "3"));
+            Assert.AreEqual(8, Program.Divide("40", "5"));
+        }
+
+        [Test]
+        public void Divide_Invalid()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        [Test]
+        public void Power_Valid()
+        {
+            Assert.AreEqual(1, Program.Power("1", "1"));
+            Assert.AreEqual(4, Program.Power("2", "2"));
+            Assert.AreEqual(16, Program.Power("4", "2"));
+        }
+
+        [Test]
+        public void Power_Invalid()
         {
             Assert.Throws<FormatException>(() => Program.Power("1", "a"));
             Assert.Throws<FormatException>(() => Program.Power("a", "1"));
@@ -48,7 +120,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Pow_Null()
+        public void Power_Null()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
