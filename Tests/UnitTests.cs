6ab96d0c1010c450fcd73b2a41b23a206b1307a3
rmartz3 <rmartz3@huskers.unl.edu>
2021-03-26 10:40:37 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ namespace TravisCILab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_Valid_Martz()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,7 +15,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_Invalid_Martz()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,7 +23,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_Null_Martz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -31,7 +31,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Subtract_Valid()
+        public void Subtract_Valid_Martz()
         {
             Assert.AreEqual(3, Program.Subtract("7", "4"));
             Assert.AreEqual(-1, Program.Subtract("3", "4"));
@@ -39,7 +39,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Subtract_Invalid()
+        public void Subtract_Invalid_Martz()
         {
             Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
             Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
@@ -47,7 +47,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Subtract_Null()
+        public void Subtract_Null_Martz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
@@ -55,7 +55,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Multiply_Valid()
+        public void Multiply_Valid_Martz()
         {
             Assert.AreEqual(28, Program.Multiply("7", "4"));
             Assert.AreEqual(-8, Program.Multiply("-2", "4"));
@@ -63,7 +63,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Multiply_Invalid()
+        public void Multiply_Invalid_Martz()
         {
             Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
             Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
@@ -71,7 +71,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Multiply_Null()
+        public void Multiply_Null_Martz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
@@ -79,7 +79,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Divide_Valid()
+        public void Divide_Valid_Martz()
         {
             Assert.AreEqual(28, Program.Divide("112", "4"));
             Assert.AreEqual(-8, Program.Divide("-32", "4"));
@@ -87,7 +87,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Divide_Invalid()
+        public void Divide_Invalid_Martz()
         {
             Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
             Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
@@ -95,7 +95,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Divide_Null()
+        public void Divide_Null_Martz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
@@ -103,7 +103,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Power_Valid()
+        public void Power_Valid_Martz()
         {
             Assert.AreEqual(16, Program.Power("2", "4"));
             Assert.AreEqual(0.25, Program.Power("4", "-1"));
@@ -111,7 +111,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Power_Invalid()
+        public void Power_Invalid_Martz()
         {
             Assert.Throws<FormatException>(() => Program.Power("1", "a"));
             Assert.Throws<FormatException>(() => Program.Power("a", "1"));
@@ -119,7 +119,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Power_Null()
+        public void Power_Null_Martz()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
