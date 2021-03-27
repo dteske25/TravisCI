80c0262923290cb7e4bd1eba3b4ed9f9886cc6bf
jamesnguyen36 <jn31346240@gmail.com>
2021-03-27 17:01:34 -0500

    Update UnitTest.cs
    Implement subtract, multiply, divide, and power tests with one failing purposely.

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,6 +30,100 @@ namespace TravisCILab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_Valid_Nguyen()
+        {
+            Assert.AreEqual(4, Program.Subtract("5", "1"));
+            Assert.AreEqual(10, Program.Subtract("20", "10"));
+            Assert.AreEqual(3, Program.Subtract("7", "4"));
+        }
+
+        [Test]
+        public void Subtract_Invalid_Nguyen()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null_Nguyen()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_Valid_Nguyen()
+        {
+            Assert.AreEqual(0, Program.Multiply("1", "0"));
+            Assert.AreEqual(30, Program.Multiply("3", "10"));
+            Assert.AreEqual(25, Program.Multiply("5", "5"));
+        }
+
+        [Test]
+        public void Multiply_Invalid_Nguyen()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_Nguyen()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_Valid_Nguyen()
+        {
+            Assert.AreEqual(1, Program.Divide("5", "5"));
+            Assert.AreEqual(30, Program.Divide("60", "2"));
+            Assert.AreEqual(6, Program.Divide("36", "6"));
+        }
+
+        [Test]
+        public void Divide_Invalid_Nguyen()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null_Nguyen()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        [Test]
+        public void Power_Valid_Nguyen()
+        {
+            Assert.AreEqual(0, Program.Power("100", "0"));
+            Assert.AreEqual(8, Program.Power("2", "3"));
+            Assert.AreEqual(1, Program.Power("1", "10"));
+        }
+
+        [Test]
+        public void Power_Invalid_Nguyen()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_Null_Nguyen()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
