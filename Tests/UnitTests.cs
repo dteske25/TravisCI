ab927889ed2f0403326109ae3b31cab7d838216f
thihoang1007 <54559318+thihoang1007@users.noreply.github.com>
2021-03-29 17:12:27 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -108,7 +108,7 @@ namespace TravisCILab
         {
             Assert.AreEqual(1, Program.Power("1", "2"));
             Assert.AreEqual(9, Program.Power("3", "2"));
-            Assert.AreEqual(12, Program.Power("5", "7"));
+            Assert.AreEqual(0, Program.Power("5", "7"));
         }
 
         [Test]
