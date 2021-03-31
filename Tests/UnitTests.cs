0bc39a6d01b373e6d50f0db022b503c21411ea98
tmcelmury <trentamcelmury@gmail.com>
2021-03-31 17:11:16 -0500

    fixed tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -11,7 +11,7 @@ namespace TravisCILab
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
-            Assert.AreEqual(10, Program.Add("5", "7"));
+            Assert.AreEqual(12, Program.Add("5", "7"));
         }
 
         [Test]
