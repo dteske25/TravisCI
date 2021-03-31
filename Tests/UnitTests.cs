1a9d49424b3e4dc9e3e55bf468d7c38b2842b670
jgarbison <54594177+jgarbison@users.noreply.github.com>
2021-03-31 12:15:05 -0500

    Added tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ namespace TravisCILab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_Valid_Garbison()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,7 +15,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_Invalid_Garbison()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,7 +23,7 @@ namespace TravisCILab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_Null_Garbison()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -31,5 +31,101 @@ namespace TravisCILab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_Valid_Garbison()
+        {
+            //This one should fail
+            Assert.AreEqual(5, Program.Subtract("5", "1"));
+            Assert.AreEqual(7, Program.Subtract("10", "3"));
+            Assert.AreEqual(1, Program.Subtract("20", "19"));
+        }
+
+        [Test]
+        public void Subtract_Invalid_Garbison()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_Null_Garbison()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multiply_Valid_Garbison()
+        {
+            Assert.AreEqual(20, Program.Multiply("5", "4"));
+            Assert.AreEqual(25, Program.Multiply("5", "5"));
+            Assert.AreEqual(4, Program.Multiply("2", "2"));
+        }
+
+        [Test]
+        public void Multiply_Invalid_Garbison()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_Null_Garbison()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Divide_Valid_Garbison()
+        {
+            Assert.AreEqual(10, Program.Divide("20", "2"));
+            Assert.AreEqual(5, Program.Divide("15", "3"));
+            Assert.AreEqual(20, Program.Divide("100", "5"));
+        }
+
+        [Test]
+        public void Divide_Invalid_Garbison()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_Null_Garbison()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        [Test]
+        public void Power_Valid_Garbison()
+        {
+            Assert.AreEqual(4, Program.Power("2", "2"));
+            Assert.AreEqual(25, Program.Power("5", "2"));
+            Assert.AreEqual(2401, Program.Power("7", "4"));
+        }
+
+        [Test]
+        public void Power_Invalid_Garbison()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_Null_Garbison()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
