58dfe3304ce617e677f7559d5a1ab5ae8ffd85ed
Ssavan99 <58150774+Ssavan99@users.noreply.github.com>
2021-03-31 23:27:10 -0500

    Fixed the test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -109,7 +109,7 @@ namespace TravisCILab
         {
             Assert.AreEqual(1, Program.Power("1", "2"));
             Assert.AreEqual(9, Program.Power("3", "2"));
-            Assert.AreEqual(1, Program.Power("5", "3"));
+            Assert.AreEqual(125, Program.Power("5", "3"));
         }
 
         [Test]
