261ef16f8e95070596ccd984287b6eb32b6bc624
Kevin Pham <kpham10@huskers.unl.edu>
2021-10-28 14:17:49 -0500

    made test cases with 1 incorrect

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,6 +30,100 @@ namespace TravisCILab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Sub_ValidPham()
+        {
+            Assert.AreEqual(10, Program.Subtract("11", "1"));
+            Assert.AreEqual(3, Program.Subtract("1", "-2"));
+            Assert.AreEqual(-4, Program.Subtract("1", "5"));
+        }
+
+        [Test]
+        public void Sub_InvalidPham()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("10", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("'", "9"));
+            Assert.Throws<FormatException>(() => Program.Subtract("||", "&&"));
+        }
+
+        [Test]
+        public void Sub_NullPham()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+
+        [Test]
+        public void Multi_ValidPham()
+        {
+            Assert.AreEqual(11, Program.Multiply("11", "1"));
+            Assert.AreEqual(-70, Program.Multiply("35", "-2"));
+            Assert.AreEqual(0, Program.Multiply("-64", "0"));
+        }
+
+        [Test]
+        public void Multi_InvalidPham()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("10", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("'", "9"));
+            Assert.Throws<FormatException>(() => Program.Multiply("||", "&&"));
+        }
+
+        [Test]
+        public void Multi_NullPham()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        [Test]
+        public void Div_ValidPham()
+        {
+            Assert.AreEqual(11, Program.Divide("11", "1"));
+            Assert.AreEqual(-15.5, Program.Divide("31", "-2"));
+            Assert.AreEqual(0.5, Program.Divide("4", "8"));
+        }
+
+        [Test]
+        public void Div_InvalidPham()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("10", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("'", "9"));
+            Assert.Throws<FormatException>(() => Program.Divide("||", "&&"));
+        }
+
+        [Test]
+        public void Div_NullPham()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        [Test]
+        public void Pow_ValidPham()
+        {
+            Assert.AreEqual(11, Program.Power("20", "1"));
+            Assert.AreEqual(4, Program.Power("16", "0.5"));
+            Assert.AreEqual(0, Program.Power("0", "8"));
+        }
+
+        [Test]
+        public void Pow_InvalidPham()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("10", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("'", "9"));
+            Assert.Throws<FormatException>(() => Program.Power("||", "&&"));
+        }
+
+        [Test]
+        public void Pow_NullPham()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
     }
 }
