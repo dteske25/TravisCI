1b7647941ced7c773d9ba43f763589c6a79c9946
unknown <katie.gerot@huskers.unl.edu>
2022-03-25 16:05:20 -0500

    Failed Unit Test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,11 +30,10 @@ namespace TravisCILab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
+        [Test]
         public void Multiply_Valid()
         {
-            Assert.AreEqual(6, Program.Add("1", "2"));
-            Assert.AreEqual(5, Program.Add("3", "2"));
-            Assert.AreEqual(12, Program.Add("5", "7"));
+            Assert.AreEqual(6, Program.Multiply("1", "2"));
         }
     }
 }
